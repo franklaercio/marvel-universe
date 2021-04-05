ee2e420609f725a7d3e6a1112c87e9d7a62fe06b
Frank Laercio <franklaercio@gmail.com>
2021-04-05 04:17:33 -0300

    Update application.properties

--- src/main/java/com/marvel/backend/SwaggerConfig.java
+++ src/main/java/com/marvel/backend/SwaggerConfig.java
@@ -1,23 +1,44 @@
 package com.marvel.backend;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.paths.RelativePathProvider;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.servlet.ServletContext;
 
 /**
  * Reference: https://reflectoring.io/documenting-spring-data-rest-api-with-springfox/
  */
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+@EnableConfigurationProperties
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    private final ServletContext servletContext;
+
+    @Autowired
+    public SwaggerConfig(ServletContext servletContext) {
+        this.servletContext = servletContext;
+    }
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .pathProvider(new RelativePathProvider(servletContext) {
+                    @Override
+                    public String getApplicationBasePath() {
+                        return "/";
+                    }
+                })
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
---
+++ src/main/java/com/marvel/backend/character/application/HomeController.java
@@ -0,0 +1,13 @@
+package com.marvel.backend.character.application;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "redirect:/swagger-ui.html";
+    }
+}
